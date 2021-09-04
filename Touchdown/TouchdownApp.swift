5e44825a19481165d0860c65e9ae154f1d5704dc
Gustavo Silva <gstvds@icloud.com>
2021-09-03 23:05:42 -0300

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Gustavo Silva on 03/09/21.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
