0e92f6f5c5a11256d1fafd5e3c398b6b41977121
Gustavo Silva <gstvds@icloud.com>
2021-09-03 23:48:11 -0300

    feat: adds featuredtabview to contentview

--- Touchdown/App/ContentView.swift
+++ Touchdown/App/ContentView.swift
@@ -19,10 +19,17 @@ struct ContentView: View {
 						.background(Color.white)
 						.shadow(color: Color.black.opacity(0.05), radius: 5, x: 0, y: 5)
 					
-					Spacer()
+					ScrollView(.vertical, showsIndicators: false, content: {
+						VStack(spacing: 0) {
+							FeaturedTabView()
+								.padding(.vertical, 20)
+								.frame(height: UIScreen.main.bounds.width / 1.475)
+							
+							FooterView()
+								.padding(.horizontal)
+						} //: VStack
+					}) //: ScrollView
 
-					FooterView()
-						.padding(.horizontal)
 				} //: VStack
 				.background(colorBackground.ignoresSafeArea(.all, edges: .all))
 			} //: ZStack
