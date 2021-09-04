c0938d5123dd50dde4178190ff4f4122fc74fede
Gustavo Silva <gstvds@icloud.com>
2021-09-04 17:10:11 -0300

    feat: adds new branditemview

---
+++ Touchdown/View/Home/BrandItemView.swift
@@ -0,0 +1,35 @@
+//
+//  BrandItemView.swift
+//  Touchdown
+//
+//  Created by Gustavo Silva on 04/09/21.
+//
+
+import SwiftUI
+
+struct BrandItemView: View {
+	// MARK: - Properties
+	let brand: Brand
+	
+	// MARK: - Body
+    var body: some View {
+			Image(brand.image)
+				.resizable()
+				.scaledToFit()
+				.padding(3)
+				.background(Color.white.cornerRadius(12))
+				.background(
+					RoundedRectangle(cornerRadius: 12).stroke(Color.gray, lineWidth: 1)
+				) //: background
+    }
+}
+
+// MARK: - Preview
+struct BrandItemView_Previews: PreviewProvider {
+    static var previews: some View {
+			BrandItemView(brand: brands[0])
+				.previewLayout(.sizeThatFits)
+				.padding()
+				.background(colorBackground)
+    }
+}
